50b5b6904354cce61e381fe2ae0f09ff02a9aedf
Michał Zychowicz <zychomichal@gmail.com>
2023-11-18 02:58:55 +0100

    Simplified bound calculating complexity

--- algorithms/BranchBound.cpp
+++ algorithms/BranchBound.cpp
@@ -1,21 +1,18 @@
-
 #include <chrono>
 #include "BranchBound.h"
 
 template<typename T>
-BranchBound<T>::BranchBound(const std::vector<std::vector<int>> &matrix)
-        : dataMatrix(matrix), numberOfCities(matrix.size()), bestBound(INT_MAX), isRunning(true) {
+BranchBound<T>::BranchBound(const std::vector<std::vector<int>>&matrix)
+    : dataMatrix(matrix), numberOfCities(matrix.size()), bestBound(INT_MAX), isRunning(true) {
     bestPathIndexes.resize(numberOfCities);
 }
+
 template<typename T>
 BranchBound<T>::~BranchBound() = default;
 
 template<typename T>
 std::tuple<int, std::vector<int>, std::chrono::duration<float>> BranchBound<T>::branchBoundAlgorithm() {
-
-
     const auto start = std::chrono::steady_clock::now();
-
 
 
     // Initialize priority queue
@@ -36,46 +33,45 @@ std::tuple<int, std::vector<int>, std::chrono::duration<float>> BranchBound<T>::
 
         // check if node can lead to better solution
         if (node.lowerBound < bestBound) {
-
-            for (auto i : node.notVisited) {
+            for (auto i: node.notVisited) {
                 // go over cities not in path
 
-                    // create child node, copy parent path and push new city to back
-                    Node childNode;
-                    childNode.path = node.path;
-                    childNode.path.push_back(i);
-                    childNode.notVisited = node.notVisited;
-                    childNode.notVisited.erase(std::ranges::find(childNode.notVisited, i));
+                // create child node, copy parent path and push new city to back
+                Node childNode;
+                childNode.path = node.path;
+                childNode.path.push_back(i);
+                childNode.notVisited = node.notVisited;
+                childNode.notVisited.erase(std::ranges::find(childNode.notVisited, i));
 
 
-                    // if all cities all already in path, add starting city to path and calculate leaf bound
-                    if (childNode.notVisited.empty()) {
-                        childNode.path.push_back(0);
-                        calculateLeafBound(childNode);
-                        // if leaf path is lower than bestBound then set bestBound to it and change best path
-                        if (childNode.lowerBound < bestBound) {
-                            bestBound = childNode.lowerBound;
-                            bestPathIndexes = childNode.path;
-                        }
+                // if all cities all already in path, add starting city to path and calculate leaf bound
+                if (childNode.notVisited.empty()) {
+                    childNode.path.push_back(0);
+                    calculateLeafBound(childNode);
+                    // if leaf path is lower than bestBound then set bestBound to it and change best path
+                    if (childNode.lowerBound < bestBound) {
+                        bestBound = childNode.lowerBound;
+                        bestPathIndexes = childNode.path;
+                    }
+                }
+                else {
+                    // calculate lowerBound of node, and if it's lower than bestBound, add it to the queue
+                    calculateLowerBound(childNode);
+                    if (childNode.lowerBound < bestBound) {
+                        queue.push(childNode);
+                    }
 
-                    } else {
-                        // calculate lowerBound of node, and if it's lower than bestBound, add it to the queue
-                        calculateLowerBound(childNode);
-                        if (childNode.lowerBound < bestBound) {
-                            queue.push(childNode);
-                        }
+#ifdef testEnable // if testEnable is defined, this code will be compiled
+                    if (!isRunning) {
+                        std::cout << "Algorithm stopped" << std::endl;
+                        throw std::runtime_error("Algorithm stopped");
+                    }
 
-                        #ifdef testEnable // if testEnable is defined, this code will be compiled
-                        if (!isRunning) {
-                            std::cout << "Algorithm stopped" << std::endl;
-                            throw std::runtime_error("Algorithm stopped");
-                        }
-
-//                        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count() > 5) {
-//                            std::cout << "Algorithm stopped" << std::endl;
-//                            throw std::runtime_error("Algorithm exceeded set time");
-//                        }
-                        #endif
+                    //                        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count() > 5) {
+                    //                            std::cout << "Algorithm stopped" << std::endl;
+                    //                            throw std::runtime_error("Algorithm exceeded set time");
+                    //                        }
+#endif
                 }
             }
         }
@@ -86,7 +82,7 @@ std::tuple<int, std::vector<int>, std::chrono::duration<float>> BranchBound<T>::
 }
 
 template<typename T>
-void BranchBound<T>::calculateLowerBound(Node &node) const {
+void BranchBound<T>::calculateLowerBound(Node&node) const {
     int lowerBound = 0;
     // find the lowerBound for partial path
     // calculate value of cities in current path
@@ -94,42 +90,39 @@ void BranchBound<T>::calculateLowerBound(Node &node) const {
         lowerBound += dataMatrix[node.path[i]][node.path[i + 1]];
     }
 
-    //
-    // std::vector<int> row;
-    // row.reserve(numberOfCities);
-    //
-    // for (const auto i : node.notVisited) {
-    //     row.push_back(dataMatrix[node.path[node.path.size() - 1]][i]);
-    // }
-    // lowerBound += *std::ranges::min_element(row);
-    //
-    //
-    // row.clear();
 
+    std::vector<int> row;
+    row.reserve(numberOfCities);
+
+    for (const auto i: node.notVisited) {
+        row.push_back(dataMatrix[node.path[node.path.size() - 1]][i]);
+    }
+    lowerBound += *std::ranges::min_element(row);
+
+
+    row.clear();
 
 
     // for last city in current path, consider path values from it to cities not in path, for cities not in path also consider path to city 0
-    for (const auto i : node.notVisited) {
+    for (const auto i: node.notVisited) {
         // if vertex not in path, excluding last element
-            std::vector<int> row;
-            row.reserve(numberOfCities);
-            for (auto j : node.notVisited) {
-                if (i != j) {
-                    if ((i == node.path[node.path.size() - 1] &&
-                         (std::ranges::find(node.path, j) == node.path.end())) ||
-                        (std::find(node.path.begin() + 1, node.path.end(), j) == node.path.end())) {
-                        row.push_back(dataMatrix[i][j]);
-                    }
+        row.reserve(numberOfCities);
+        for (auto j: node.notVisited) {
+            if (i != j) {
+                if (std::find(node.path.begin() + 1, node.path.end(), j) == node.path.end()) {
+                    row.push_back(dataMatrix[i][j]);
                 }
             }
-            lowerBound += *std::ranges::min_element(row);
+        }
+        lowerBound += *std::ranges::min_element(row);
+        row.clear();
     }
 
     node.lowerBound = lowerBound;
 }
 
 template<typename T>
-void BranchBound<T>::calculateLeafBound(Node &node) const {
+void BranchBound<T>::calculateLeafBound(Node&node) const {
     // calculate distance between all cities in path
     int lowerBound = 0;
     for (int i = 0; i < node.path.size() - 1; ++i) {
@@ -139,7 +132,7 @@ void BranchBound<T>::calculateLeafBound(Node &node) const {
 }
 
 template<typename T>
-void BranchBound<T>::calculateRootBound(Node &node) const {
+void BranchBound<T>::calculateRootBound(Node&node) const {
     // calculate lower bound of root node
     int lowerBound = 0;
     // find minimum value in each row and add it to lowerBound
@@ -155,11 +148,9 @@ void BranchBound<T>::calculateRootBound(Node &node) const {
         lowerBound += *std::ranges::min_element(row);
     }
     node.lowerBound = lowerBound;
-
 }
 
 // template is limited to using stack or priority queue
 template class BranchBound<std::stack<Node>>;
 //template class BranchBound<std::priority_queue<Node, std::vector<Node>, compareNodes>>;
 template class BranchBound<nodePriorityQueue>;
-
